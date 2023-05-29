f6eecf56c09bd2445b560e316a16767c718aea0b
Sunho <sunh803@naver.com>
2023-05-29 11:53:57 +0900

    Initial Commit

---
+++ Moviestar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Moviestar
+//
+//  Created by 235 on 2023/05/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
